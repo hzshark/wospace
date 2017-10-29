28aa2c98cd7c9f24228d9e89c8ee4ac0ef9b41de
hzshark <hzshark@qq.com>
2017-10-29 14:20:11 +0800

    add

---
+++ src/main/java/cn/netdisk/wospace/WospaceApplication.java
@@ -0,0 +1,12 @@
+package cn.netdisk.wospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WospaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WospaceApplication.class, args);
+	}
+}
